7acc1744f729da25f10a28f13e72569ee9df0ed4
skwasha <sascha@umich.edu>
2016-08-08 00:10:44 -0700

    change config url

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -3,7 +3,7 @@ Pebble.addEventListener('ready', function() {
 });
 
 Pebble.addEventListener('showConfiguration', function() {
-  var url = 'https://17168558.ngrok.io';
+  var url = 'https://skwasha.github.io/Skwatch/';
 
   console.log('Showing configuration page: ' + url);
 
