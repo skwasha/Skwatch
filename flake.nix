de5dc9d713ed874b576389cfb98c9146c9b67261
Sascha Linn <sascha@umich.edu>
2025-02-23 23:03:13 -0800

    added flake.nix

---
+++ flake.nix
@@ -0,0 +1,12 @@
+{
+  inputs = {
+    pebble.url = "github:Sorixelle/pebble.nix";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { pebble, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system: {
+      devShell = pebble.pebbleEnv.${system} { };
+    });
+}
